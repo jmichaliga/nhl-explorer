388f8dbc757268387e5b2f47c547c5a952a9bf2e
Justin Michaliga <justin@capsule8.com>
2017-10-19 23:37:04 -0400

    initial commit :zap:

---
+++ pages/standings.vue
@@ -0,0 +1,20 @@
+<template>
+  <code>{{ nhl_data }}</code>
+</template>
+
+<script>
+export default {
+  components: {
+    Logo
+  },
+  data () {
+    return {
+      nhl_data: 'https://statsapi.web.nhl.com/api/v1/schedule?startDate=2017-01-01&endDate=2017-02-01&expand=schedule.teams,schedule.linescore,schedule.broadcasts,schedule.ticket,schedule.game.content.media.epg&leaderCategories=&site=en_nhl&teamId=5'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
