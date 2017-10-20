52f36b0e56548fcd47ba4c66e9eaf7eae2b271cd
Justin Michaliga <justin@capsule8.com>
2017-10-20 10:38:32 -0400

    feature: added support for player data and rosters from teams

---
+++ pages/players/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>Player:</h2>
+    <pre>{{ player.people }}</pre>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData ({ params, error }) {
+    console.log('params:', params)
+    try {
+      const { data } = await axios.get('https://statsapi.web.nhl.com/api/v1/people/' + params.id)
+      return { player: data }
+    } catch (e) {
+      console.warn({ message: 'Player not Found', statusCode: 404 })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/teams/_id.vue
+++ pages/teams/_id.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
     <h2>Team:</h2>
-    <pre>{{ team }}</pre>
+    <!-- <pre>{{ team.roster }}</pre> -->
+
+    <ul class="roster">
+      <li class="player" v-for="player in team.roster">
+        {{player.jerseyNumber}}: <a :href="'../players/'+player.person.id">{{player.person.fullName}}</a>, {{player.position.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,8 +17,8 @@ export default {
   async asyncData ({ params, error }) {
     console.log('params:', params)
     try {
-      const { data } = await axios.get('https://statsapi.web.nhl.com/api/v1/teams/' + params.id)
-      return { team: data.teams[0] }
+      const { data } = await axios.get('https://statsapi.web.nhl.com/api/v1/teams/' + params.id + '/roster')
+      return { team: data }
     } catch (e) {
       console.warn({ message: 'Team no Found', statusCode: 404 })
     }
